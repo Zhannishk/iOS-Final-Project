8f3be582310dccf811204e8a9094390829f85514
Zhannishk <janiaa.uk@gmail.com>
2025-12-17 16:11:15 +0500

    Reminders UI

--- Tracker/Tracker/Controllers/ReminderViewController.swift
+++ Tracker/Tracker/Controllers/ReminderViewController.swift
@@ -5,3 +5,32 @@
 //  Created by Zhalgas Bagytzhan on 16.12.2025.
 //
 
+import UIKit
+
+class ReminderViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+//        tableView.delegate = self
+    }
+    
+    
+}
+extension ReminderViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Event #\(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "24 Dec 2025"
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
