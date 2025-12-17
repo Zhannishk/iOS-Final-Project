95f76997f4695f26a45f50f883ce04ee00e1063c
zhokhanty <z_bagytzhan@kbtu.kz>
2025-12-17 08:23:13 +0500

    iOS

--- Tracker/Tracker/Models/ActivityModel.swift
+++ Tracker/Tracker/Models/ActivityModel.swift
@@ -9,5 +9,29 @@
 struct ActivityModel: Codable {
     let activityName: String
     let type: String
-    let duration: String
+    let participants: Int
+    let price: Double
+    let link: String
+    let key: String
+    let accessibility: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case activityName = "activity"
+        case type
+        case participants
+        case price
+        case link
+        case key
+        case accessibility
+    }
+}
+
+enum ActivityType: String, Codable {
+    case education
+    case recreational
+    case social
+    case charity
+    case cooking
+    case relaxation
+    case busywork
 }
